45a751a3afa017b9db8fbae9e5d41a4b99955502
olsonkyle <A02316340@usu.edu>
2022-11-10 17:23:47 -0700

    huge upgrade
    jquery and more media queries

---
+++ frankflowerswebsite/js/overlay.js
@@ -0,0 +1,11 @@
+ /* Open when user clicks on the span element */
+ function openNav() {
+    document.getElementById('myNav').style.display = 'block';
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when user clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById('myNav').style.display = 'none';
+    document.getElementById("myNav").style.width = "0%";
+}
