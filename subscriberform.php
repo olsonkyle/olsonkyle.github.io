e2f6a5190242718375448408601942c5e99cfdea
olsonkyle <A02316340@usu.edu>
2022-11-20 15:55:58 -0700

    trying to fix carousel

--- subscriberform.php
+++ subscriberform.php
@@ -1,6 +1,32 @@
 <?php
-if($_POST["myEmail"]) {
-mail("frankflowers21@gmail.com", "ADD ME TO THE LIST",
-$_POST["Insert Your Message"]. "From: frankflowers21@gmail.com");
-}
+
+## CONFIG ##
+
+# LIST EMAIL ADDRESS
+$recipient = "frankflowers21@gmail.com";
+
+# SUBJECT (Subscribe/Remove)
+$subject = "NEW SUBSCRIBER";
+
+# RESULT PAGE
+# $location = "enter the URL of the result page here";
+
+## FORM VALUES ##
+
+# SENDER - WE ALSO USE THE RECIPIENT AS SENDER IN THIS SAMPLE
+# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
+$sender = $recipient;
+
+# MAIL BODY
+$body .= "Name: ".$_REQUEST['Name']." \n";
+$body .= "Email: ".$_REQUEST['Email']." \n";
+# add more fields here if required
+
+## SEND MESSGAE ##
+
+mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+
+## SHOW RESULT PAGE ##
+
+# header( "Location: $location" );
 ?>
